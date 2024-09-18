ab8e5c1d06258cb9f8ddb082def4730bb9c988d3
danchous21 <danceussisk228@gmail.com>
2024-09-18 21:15:02 +0300

    Создал формы и представления для юзера и поста

--- mysocialnetwork/posts/views.py
+++ mysocialnetwork/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
-# Create your views here.
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # Сохраняем пост, но не отправляем в базу данных
+            post.author = request.user  # Привязываем пост к текущему авторизованному пользователю
+            post.save()  # Сохраняем пост в базе данных
+            return redirect('home')
+    else:
+        form = PostForm()  # Если запрос GET, создаём пустую форму
+    return render(request, 'posts/create_post.html', {'form': form})
--- mysocialnetwork/users/views.py
+++ mysocialnetwork/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)  # Обработка данных формы и загружаемых файлов
+        if form.is_valid():
+            user = form.save()  # Сохранение нового пользователя
+            login(request, user)  # Автоматический вход пользователя в систему
+            return redirect('home')  # Перенаправление на главную страницу
+    else:
+        form = CustomUserCreationForm()  # Если запрос GET, создаётся пустая форма
+    return render(request, 'users/register.html', {'form': form})  # Отображение шаблона с формой
