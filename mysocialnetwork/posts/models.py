8fb41a81b4f53723950314ceb7f67f3844bce01a
danchous21 <danceussisk228@gmail.com>
2024-09-18 20:33:42 +0300

    обновил модели и админку

--- mysocialnetwork/posts/models.py
+++ mysocialnetwork/posts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
